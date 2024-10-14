454dafeab7bf9408418f8674d35d8c57dbe6db6f
Swastik Pratik <swastiksingh368@gmail.com>
2024-10-14 11:50:52 +0530

    p2p trascation and fixed some

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,23 +3,15 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-    try {
-        const session = await getServerSession(authOptions);
-        if (session.user) {
-            return NextResponse.json({
-                user: session.user
-            })
-        }
+    const session = await getServerSession(authOptions);
+    if (session.user) {
         return NextResponse.json({
-            message: "You are not logged in"
-        }, {
-            status: 403
-        })
-    } catch (error) {
-        return NextResponse.json({
-            message: "You are not logged in"
-        }, {
-            status: 403
+            user: session.user
         })
     }
+    return NextResponse.json({
+        message: "You are not logged in"
+    }, {
+        status: 403
+    })
 }
